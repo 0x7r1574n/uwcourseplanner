0b477a3f5b705118d3987be9c492456a206ad2a9
Alex Lew <alew104@gmail.com>
2015-08-15 23:22:15 -0700

    fix qtr

--- planner/models.py
+++ planner/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-
-class IntegerRangeField(models.IntegerField):
-    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
-        self.min_value, self.max_value = min_value, max_value
-        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
-    def formfield(self, **kwargs):
-        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
-        defaults.update(kwargs)
-        return super(IntegerRangeField, self).formfield(**defaults)
-
 class Course(models.Model):
     user = models.ForeignKey('auth.User')
     fullname = models.CharField(max_length=10)
@@ -19,6 +8,6 @@ class Course(models.Model):
     number = models.IntegerField()
     credit = models.IntegerField()
     year = models.IntegerField()
-    quarter = fields.IntegerRangeField(min_value=1, max_value=4)
+    quarter = models.IntegerField()
     title = models.CharField(max_length=50)
     description = models.TextField()
