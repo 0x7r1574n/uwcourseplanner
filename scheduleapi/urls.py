87290f43b698aaa58cca67dfee7fe01c9d9a775f
Cechi Shi <shicechi@gmail.com>
2015-08-16 00:49:56 -0700

    update for new view

--- scheduleapi/urls.py
+++ scheduleapi/urls.py
@@ -3,8 +3,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from scheduleapi import views
 
 urlpatterns = [
-    url(r'post/$', views.CourseCreate.as_view(), name='course-create'),
-    url(r'^courses/(?P<pk>[0-9]+)/$', views.CourseDetail.as_view(), name='course-detail'),
+    url(r'post/$', views.CourseList.as_view(), name='course-list'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
