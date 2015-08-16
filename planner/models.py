ea407ad1a236031957e05cd225bb8aaf480f5713
Alex Lew <alew104@gmail.com>
2015-08-15 23:12:04 -0700

    try this on for size
    limit quarter
    1 = aut
    2 = win
    3 = sprng
    4 = sum
    
    sort year 1 into quarters test code

--- planner/models.py
+++ planner/models.py
@@ -2,13 +2,23 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+
+class IntegerRangeField(models.IntegerField):
+    def __init__(self, verbose_name=None, name=None, min_value=None, max_value=None, **kwargs):
+        self.min_value, self.max_value = min_value, max_value
+        models.IntegerField.__init__(self, verbose_name, name, **kwargs)
+    def formfield(self, **kwargs):
+        defaults = {'min_value': self.min_value, 'max_value':self.max_value}
+        defaults.update(kwargs)
+        return super(IntegerRangeField, self).formfield(**defaults)
+
 class Course(models.Model):
     user = models.ForeignKey('auth.User')
-    fullName = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=10)
     dept = models.CharField(max_length=10)
     number = models.IntegerField()
     credit = models.IntegerField()
     year = models.IntegerField()
-    quarter = models.CharField(max_length=10)
+    quarter = fields.IntegerRangeField(min_value=1, max_value=4)
     title = models.CharField(max_length=50)
     description = models.TextField()
--- planner/views.py
+++ planner/views.py
@@ -3,13 +3,19 @@ from django.utils import timezone
 from .models import Course
 from django.shortcuts import render, get_object_or_404
 from .forms import CourseForm
+
 from django.shortcuts import redirect
 # Create your views here.
 
 
 def course_list(request):
     courses = Course.objects.filter(author=user).order_by('year')
-    return render(request, 'planner/course_list.html', {'courses' : courses})
+    year1 = courses.filter(year=1)
+    y1q1 = year1.filter(quarter=1)
+    y1q2 = year1.filter(quarter=2)
+    y1q3 = year1.filter(quarter=3)
+    y1q4 = year1.filter(quarter=4)
+    return render(request, 'planner/course_list.html', {'courses' : courses}, {'y1' : year1}, {'y1q1' : y1q1}, {'y1q2' : y1q2}, {'y1q3' : y1q3}, {'y1q4' : y1q4})
 
 def course_new(request):
     if request.method == "POST":
