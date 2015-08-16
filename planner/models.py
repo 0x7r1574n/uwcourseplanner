296916cb2897760ee5f3c131ef80f3a9a1c7c430
Cechi Shi <shicechi@gmail.com>
2015-08-15 17:26:31 -0700

    first model try

--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    user = models.ForeignKey('auth.User')
+    fullName = models.CharField(max_length=10)
+    dept = models.CharField(max_length=10)
+    number = models.IntegerField()
+    credit = models.IntegerField()
+    year = models.IntegerField()
+    quarter = models.CharField(max_length=10)
+    title = models.CharField(max_length=50)
+    description = models.TextField()
