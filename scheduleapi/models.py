0ac335515d47780d0fa9734fe66502e00e78ad48
Cechi Shi <shicechi@gmail.com>
2015-08-15 23:01:42 -0700

    removed credits field

--- scheduleapi/models.py
+++ scheduleapi/models.py
@@ -1,11 +1,8 @@
 from django.db import models
-import requests
-import bs4
 
 
 class Course(models.Model):
-    fullName = models.CharField(max_length=10)
+    fullname = models.CharField(max_length=10)
     dept = models.CharField(max_length=10)
     number = models.IntegerField()
-    credit = models.IntegerField()
     title = models.CharField(max_length=50)
