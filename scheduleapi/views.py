8c646a31fe51f6b5922114fce0163d45db63dd23
Cechi Shi <shicechi@gmail.com>
2015-08-15 23:02:57 -0700

    updated views

--- scheduleapi/views.py
+++ scheduleapi/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from .models import Course
+from .serializers import CourseSerializer
+from rest_framework import generics
 
-# Create your views here.
+
+class CourseCreate(generics.CreateAPIView):
+    serializer_class = CourseSerializer
+    model = Course
+
+
+class CourseDetail(generics.RetrieveAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
