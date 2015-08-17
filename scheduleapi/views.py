21b054f137ed6435e2342b8c9081212c38884021
Cechi Shi <shicechi@gmail.com>
2015-08-16 19:38:22 -0700

    added filter set for multiple params

--- scheduleapi/views.py
+++ scheduleapi/views.py
@@ -1,3 +1,4 @@
+import django_filters
 from .models import Course
 from .serializers import CourseSerializer
 from rest_framework import generics
@@ -6,3 +7,9 @@ from rest_framework import generics
 class CourseList(generics.ListAPIView):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class CourseFilter(django_filters.FilterSet):
+    class Meta:
+        model = Course
+        fields = ['fullname', 'dept', 'number', 'title']
