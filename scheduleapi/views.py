cc12dc06d90b30cfc891cb89e189888d414d48c5
Cechi Shi <shicechi@gmail.com>
2015-08-16 19:40:46 -0700

    add filter class to view

--- scheduleapi/views.py
+++ scheduleapi/views.py
@@ -4,12 +4,13 @@ from .serializers import CourseSerializer
 from rest_framework import generics
 
 
-class CourseList(generics.ListAPIView):
-    queryset = Course.objects.all()
-    serializer_class = CourseSerializer
-
-
 class CourseFilter(django_filters.FilterSet):
     class Meta:
         model = Course
         fields = ['fullname', 'dept', 'number', 'title']
+
+
+class CourseList(generics.ListAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+    filter_class = CourseFilter
