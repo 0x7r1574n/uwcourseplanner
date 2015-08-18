91e279f999c73df2aad0433ebc9689669cd5a486
Alex Lew <alew104@gmail.com>
2015-08-17 18:11:20 -0700

    fix extra slash

--- planner/urls.py
+++ planner/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^planner/(?P<pk>[0-9]+)/$', views.course_detail, name='course_detail'),
     url(r'^planner/new/$', views.course_new, name='course_new'),
     url(r'^planner/(?P<pk>[0-9]+)/edit/$', views.course_edit, name='course_edit'),
-    url(r'^/accounts/login/$', 'django.contrib.auth.views.login'),
-    url(r'^/accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/urlexpand'}),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/urlexpand'}),
 ]
