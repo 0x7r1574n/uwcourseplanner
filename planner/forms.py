6e6d8854d4b41c9af95e32848238fe42940224f2
Cechi Shi <shicechi@gmail.com>
2015-08-18 00:58:49 -0700

    remove fullname field

--- planner/forms.py
+++ planner/forms.py
@@ -1,8 +1,8 @@
 from django import forms
 from .models import Course
 
-class CourseForm(forms.ModelForm):
 
-	class Meta:
-		model = Course
-		fields = ('fullname', 'dept', 'number', 'year', 'quarter', 'credit')
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ('dept', 'number', 'year', 'quarter', 'credit')
