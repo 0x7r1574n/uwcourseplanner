892af3b7d729d1fa585f40cc4c1da9cf9bc6bed9
Cechi Shi <shicechi@gmail.com>
2015-08-18 00:59:09 -0700

    auto generate fullname field

--- planner/models.py
+++ planner/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Course(models.Model):
     user = models.ForeignKey('auth.User')
-    fullname = models.CharField(max_length=10)
-    dept = models.CharField(max_length=10)
+    dept = models.CharField(max_length=50)
     number = models.IntegerField()
+    fullname = dept.lower().replace(' ', '') + str(number)
     credit = models.IntegerField()
     year = models.IntegerField()
     quarter = models.IntegerField()
-    title = models.CharField(max_length=50)
-    description = models.TextField()
+    title = models.CharField(max_length=150)
