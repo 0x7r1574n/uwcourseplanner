cc7e2b821f2526e2c5c8d366db5f059b386cb996
Cechi Shi <shicechi@gmail.com>
2015-08-23 16:29:07 -0700

    added min values for integer fields

--- planner/forms.py
+++ planner/forms.py
@@ -4,9 +4,9 @@ from .models import Course
 
 class CourseForm(forms.ModelForm):
     fullname = forms.CharField(max_length=50)
-    year = forms.IntegerField(max_value=4)
-    quarter = forms.IntegerField(max_value=4)
-    credit = forms.IntegerField(max_value=20)
+    year = forms.IntegerField(max_value=4, min_value=1)
+    quarter = forms.IntegerField(max_value=4, min_value=1)
+    credit = forms.IntegerField(max_value=20, min_value=1)
 
     class Meta:
         model = Course
