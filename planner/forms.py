3d194dad9c92b8f64253077c19361d282d08d6ed
Cechi Shi <shicechi@gmail.com>
2015-08-23 15:32:21 -0700

    added initial form validation

--- planner/forms.py
+++ planner/forms.py
@@ -3,6 +3,11 @@ from .models import Course
 
 
 class CourseForm(forms.ModelForm):
+    fullname = forms.CharField(max_length=50)
+    year = forms.IntegerField(max_value=4)
+    quarter = forms.IntegerField(max_value=4)
+    credit = forms.IntegerField(max_value=20)
+
     class Meta:
         model = Course
         fields = ('fullname', 'year', 'quarter', 'credit')
